fc4ef54695e4f4a76de0915bece433ffac0243b3
thierri oliveira <thierrioliveira@MacBook-Pro-de-thierri.local>
2022-11-06 15:33:02 -0300

    pronto

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-console.log('HI')
+import express from "express"
+import cors from "cors"
 
+const app = express()
+
+const dados = []
+
+app.use(express.json({ type: ['text/*', '*/json'] }))
+app.use(cors())
+
+app.get('/', (request, response) => {
+    console.log('Endpoint get solicitado!!')
+    response.send(dados)
+})
+
+app.post('/', (request, response) => {
+    console.log('Endpoint POST solicitado!!')
+    console.log(request.body)
+    dados.push({
+        message: request.body.Message,
+        timestamp: Date.now()
+    })
+    response.send('OI!')
+})
+
+
+app.listen(3000, () => {
+    console.log('Microservico Rodando!')
+})
